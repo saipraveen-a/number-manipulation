b6bfd6f3f2186fd2c5f6b6537c7df20f877fd898
Sai Praveen Anumukonda <saipraveen.a.iiit@gmail.com>
2020-07-06 12:55:22 +0530

    Function to add 2 integers

---
+++ calc/Math.go
@@ -0,0 +1,6 @@
+package calc
+
+// Add - Returns sum of given 2 integers
+func Add(i int, j int) int {
+	return i + j
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saipraveen-a/number-manipulation
+
+go 1.14
